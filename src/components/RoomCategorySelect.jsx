7d06e5d283db2b28d1d06a5f6ddfa7c7eeb2cc53
apradip <pradip.adhikary@gmail.com>
2023-02-14 11:21:59 +0530

    fix

--- src/components/RoomCategorySelect.jsx
+++ src/components/RoomCategorySelect.jsx
@@ -1,13 +1,12 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect } from "react";
 
-import { HotelId } from "../App"
+import { HotelId } from "../App";
 import { useStateContext } from "../contexts/ContextProvider";
-import useFetchWithAuth from "./useFetchWithAuth"
+import useFetchWithAuth from "./useFetchWithAuth";
 
 const RoomCategorySelect = ({ onChange, name, value, disabled = false }) => {
-	const hotelId = useContext(HotelId)
+	const hotelId = useContext(HotelId);
 	const contextValues = useStateContext();
-	// const inputRef = useRef();
     const { data, loading, doFetch } = useFetchWithAuth({
         url: `${contextValues.roomCategoryAPI}/${hotelId}`
     });
@@ -24,29 +23,23 @@ const RoomCategorySelect = ({ onChange, name, value, disabled = false }) => {
     }, [doFetch]);
     // End:: fetch data list from api
 
-    // useEffect(() => {
-	// 	!loading && inputRef.current.focus();
-    // }, [data, loading, error]);
-
 	return (
 		<select 
 			className="form-control"
 			autoFocus
-			name = { name }
-			// ref = { inputRef }
-			disabled = { loading && disabled } 
-			value = { value } 
-			onChange = { (e) => { onChange(e.target.value) } } >
+			name={name}
+			disabled={loading && disabled} 
+			value={value} 
+			onChange={(e) => {onChange(e.target.value)}} >
 
 			<option key="" value="">Select category</option>
 			
-			{ data &&
+			{data &&
 				data.map((item) => {
 					return (<option key = { item._id } value = { item._id } >{ item.name }</option>)
-				}) }
-
+				})}
 		</select>
     );
-}
+};
  
-export default RoomCategorySelect
+export default RoomCategorySelect;
