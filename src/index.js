409422d594cdd342cc83ef13d7e39cfdd458fc0a
apradip <pradip.adhikary@gmail.com>
2023-09-03 15:36:01 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-
 import "bootstrap/dist/css/bootstrap.css";  
 import "overlayscrollbars/overlayscrollbars.css";
 import "react-toastify/dist/ReactToastify.css";
